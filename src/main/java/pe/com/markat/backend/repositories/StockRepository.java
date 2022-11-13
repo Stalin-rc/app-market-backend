1373acfe850b278efe2191af9971f0c2cbc51809
u201821671 <111545448+u201821671@users.noreply.github.com>
2022-11-13 00:55:49 -0500

    Implementación de querys para dashboard

--- src/main/java/pe/com/markat/backend/repositories/StockRepository.java
+++ src/main/java/pe/com/markat/backend/repositories/StockRepository.java
@@ -7,10 +7,10 @@ import pe.com.markat.backend.entities.Stock;
 import java.util.List;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
-    @Query("select s from Stock s where s.store.id=?1 order by s.noUnits desc")
+    @Query("select s from Stock s where s.store.id=?1 order by s.noUnits asc")
     List<Stock> findAllStocksByStoreIdJPA(Long id);
 
-    @Query("SELECT s FROM Stock s WHERE s.store.id =?1 AND s.noUnits < 20 order by s.noUnits asc")
+    @Query("SELECT s FROM Stock s WHERE s.store.id =?1 AND s.noUnits < 20 order by s.noUnits desc")
     List<Stock> findStocksByStoreIdJPA(Long id);
 
     @Query("SELECT SUM(c.noUnits) FROM Stock c WHERE c.store.id=?1")
